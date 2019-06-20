226122848b816b37b64c561a3486d21f02ef8509
zimnerd <eddienyagano@gmail.com>
2019-06-20 09:35:35 +0200

    finished http requests

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 })
 
+app.get('/users',function (request,res) {
+  res.json([
+    {"id":1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id":2,"firstName":"Bobby","lastName":"Smit","email":"bob2@gmail.com"},
+    {"id":2,"firstName":"Bobbie","lastName":"Smut","email":"bob3@gmail.com"}
+  ])
+
+})
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import './index.css'
-import numeral from 'numeral'
-/* eslint-disable no-console */
-const courseValue = numeral(1000).format('$0,0.00')
-console.log(`I would pay ${courseValue} for this awesome course!`)
+import './assets/css/bootstrap.min.css'
+import './assets/css/bootstrap-grid.min.css'
+import './assets/css/bootstrap-reboot.min.css'
+import {deleteUser,getUsers} from "./api/apiUsers";
+getUsers().then(result => {
+  console.log(result);//eslint-disable-line no-console
+  let userBody = "";
+  result.forEach(user => {
+    userBody += `<tr>
+<td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a> </td>
+<td>${user.id}</td><td>${user.firstName}</td><td>${user.lastName}</td><td>${user.email}</td>
+</tr>`;
+  });
+
+  global.document.getElementById("users").innerHTML = userBody;
+
+  const deleteLinks = global.document.getElementsByClassName("deleteUser");
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
