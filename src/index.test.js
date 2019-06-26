fd8572c22299556d82dd5caa9ab38dde60ab3fba
zimnerd <eddienyagano@gmail.com>
2019-06-26 09:24:34 +0200

    FInal edit with console

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
 import {expect} from 'chai';
 import jsdom from 'jsdom';
-import fs from 'fs';
+import fs from 'fsss';
 
 
 describe('Our first test', () => {
@@ -15,7 +15,7 @@ describe('index.html', () => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function (err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Users");
+      expect(h1.innerHTML).to.equal("User");
       done();
 
     })
