44d4378c97a1a30b4d7b52959201f953a7de5a68
zimnerd <eddienyagano@gmail.com>
2019-06-26 09:15:19 +0200

    FInal edit with console

--- src/index.js
+++ src/index.js
@@ -14,9 +14,8 @@ getUsers().then(result => {
   });
 
   global.document.getElementById("users").innerHTML = userBody;
-
+console.log("here");
   const deleteLinks = global.document.getElementsByClassName("deleteUser");
-
   // Must use array.from to create a real array from a DOM collection
   // getElementsByClassname only returns an "array like" object
   Array.from(deleteLinks, link => {
